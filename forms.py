b476c888c8c3bd0f9df7dbeb7f705e06cef04096
NikPotapov <95213401+NikPotapov@users.noreply.github.com>
2023-10-05 11:10:47 +0200

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-
-from django import forms
-
-class YourForm(forms.Form):
-    your_field = forms.CharField(label='What name of the book do you like?',
-                                  widget=forms.TextInput(attrs={'autocomplete': 'off'}))
